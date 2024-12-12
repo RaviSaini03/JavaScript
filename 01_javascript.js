787aafd5d2150e5fd5f7f2c10e51a55ea68fca4b
Ravi Saini <ravisainipaptawan030504@gmail.com>
2024-12-12 13:16:30 +0530

    variables
    day 1/20 of 20DaysOfJavaScript

---
+++ 01_javascript.js
@@ -0,0 +1,9 @@
+const studentName = "Ravi Saini";
+let studentId = "ravisaini@gmail.com";
+var studentNumber = "15";
+studentBranch = "CSE";
+
+// studentName = "Rohan" // can't do this because you used const variable
+
+console.log(studentName);
+console.table[studentName, studentId, studentNumber, studentBranch];
